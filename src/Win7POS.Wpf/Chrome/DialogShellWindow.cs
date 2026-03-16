c4888cbdb45f04e5b0bd402aed888336eb8f680b
minxiang <xniw97@gmail.com>
2026-03-15 22:02:37 -0300

    UI 12

--- src/Win7POS.Wpf/Chrome/DialogShellWindow.cs
+++ src/Win7POS.Wpf/Chrome/DialogShellWindow.cs
@@ -213,7 +213,7 @@ namespace Win7POS.Wpf.Chrome
 
             var contentHost = new ContentPresenter
             {
-                Margin = new Thickness(20)
+                Margin = new Thickness(24, -2, 24, 0)
             };
             contentHost.SetBinding(ContentPresenter.ContentProperty, new Binding("DialogContent") { Source = this });
             Grid.SetRow(contentHost, 1);
